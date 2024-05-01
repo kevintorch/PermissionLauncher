4a36cc8578d95e68537d4ad37c85ace718d55532
Karan Kumar (New) <karan.kumar@zimong.com>
2024-05-01 20:11:21 +0530

    - Small Fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("com.github.kevintorch:PermissionLauncher:1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
