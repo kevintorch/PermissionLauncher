c13d2d7f2d66cbeee21a1a5f5e55ab996ae0fb10
Karan Kumar (New) <karan.kumar@zimong.com>
2024-05-01 14:46:36 +0530

    Initial commit

---
+++ permission-launcher/src/main/java/com/kevintorch/permission_launcher/PermissionLauncher.kt
@@ -0,0 +1,101 @@
+package com.kevintorch.permission_launcher
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class PermissionLauncher {
+    private var activity: AppCompatActivity? = null
+    private val callback: ActivityResultCallback<Boolean>
+    private val launcher: ActivityResultLauncher<String>
+    private var rationaleMessage: CharSequence? = null
+
+    private var enableRationale = true
+
+    constructor(activity: AppCompatActivity, callback: ActivityResultCallback<Boolean>) {
+        this.activity = activity
+        this.callback = callback
+        launcher = activity.registerForActivityResult(RequestPermission(), callback)
+    }
+
+    constructor(fragment: Fragment, callback: ActivityResultCallback<Boolean>) {
+        this.activity = fragment.activity as AppCompatActivity?
+        this.callback = callback
+        launcher = fragment.registerForActivityResult(RequestPermission(), callback)
+    }
+
+    constructor(activity: AppCompatActivity, onGranted: Runnable) {
+        this.activity = activity
+        this.callback = ActivityResultCallback { granted: Boolean ->
+            if (granted) onGranted.run()
+        }
+        launcher = activity.registerForActivityResult(RequestPermission(), this.callback)
+    }
+
+    constructor(fragment: Fragment, onGranted: Runnable) {
+        if (fragment.activity != null) {
+            activity = fragment.activity as? AppCompatActivity
+        } else {
+            fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                override fun onCreate(owner: LifecycleOwner) {
+                    activity = fragment.activity as? AppCompatActivity
+                }
+            })
+        }
+        this.callback = ActivityResultCallback { granted -> if (granted) onGranted.run() }
+        launcher = fragment.registerForActivityResult(RequestPermission(), this.callback)
+    }
+
+    @JvmOverloads
+    fun setEnableRationale(enableRationale: Boolean, rationaleMessage: CharSequence? = null) {
+        this.enableRationale = enableRationale
+        this.rationaleMessage = rationaleMessage
+    }
+
+    fun launch(permission: String) {
+        if (activity == null) {
+            throw IllegalArgumentException("Activity is null")
+        }
+        if (isPermissionsGranted(activity!!, permission)) {
+            callback.onActivityResult(true)
+            return
+        }
+        if (enableRationale && ActivityCompat.shouldShowRequestPermissionRationale(
+                activity!!,
+                permission
+            )
+        ) {
+            // Display a dialog with rationale.
+            RationaleAlertDialog(activity!!, permission).show()
+        } else {
+            launcher.launch(permission)
+        }
+    }
+
+    private fun isPermissionsGranted(activity: AppCompatActivity, permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(activity, permission) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private inner class RationaleAlertDialog(context: Context, permission: String) :
+        MaterialAlertDialogBuilder(context) {
+        init {
+            setTitle("Permission Required")
+            setMessage(rationaleMessage ?: "$permission is required to work properly.")
+            setPositiveButton("Allow") { dialog: DialogInterface?, which: Int ->
+                launch(permission)
+            }
+            setNegativeButton("No Thanks", null)
+        }
+    }
+}
